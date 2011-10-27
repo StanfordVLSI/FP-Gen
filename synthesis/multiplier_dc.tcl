94f4a5ab6023ba5a604c369a8d9de7cc1c294b20
sameh06 <sameh06@sameh06p4>
2011-10-27 15:35:52 -0800

    - Modified booth y edge signal to mean y[i]!=y[i+1]        - Fixed bug in booth 3 implementation (extra bit in mux_control signal)        - some updates to synthesis stuff
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 9657]

--- synthesis/multiplier_dc.tcl
+++ synthesis/multiplier_dc.tcl
@@ -1,13 +1,21 @@
-set DESIGN_NAME Multiplier_unq1
+source header.tcl
 
 analyze -format sverilog [glob ../*.v]
 elaborate $DESIGN_NAME -architecture verilog -library DEFAULT
-
-set_max_delay -from [all_inputs] -to [all_outputs] 1
-
+set_max_delay -from [all_inputs] -to [all_outputs] 0
+set_dont_touch [get_cells -hierarchical "add"]
+set_dont_touch [get_cells -hierarchical "cell_*"]
+#set_map_only $CSA_cells
 compile_ultra -no_autoungroup
 
-write -format verilog -hierarchy -output $DESIGN_NAME.mapped.v
-write_sdc -nosplit $DESIGN_NAME.mapped.sdc
+set CSA_cells [get_cells -hierarchical "*add"];
+remove_attribute -quiet $CSA_cells dont_touch
+set_size_only $CSA_cells
+compile_ultra -no_autoungroup
+
+write -format verilog -hierarchy -output $DESIGN_NAME.$VT.mapped.v
+write -format ddc -hierarchy -output $DESIGN_NAME.$VT.mapped.ddc
+write_sdc -nosplit $DESIGN_NAME.$VT.mapped.sdc
+report_timing -transition_time -nets -attributes -nosplit > reports/${DESIGN_NAME}.${VT}_$Voltage.mapped.timing.rpt
 
 exit
--- synthesis/multiplier_icc.tcl
+++ synthesis/multiplier_icc.tcl
@@ -1,6 +1,15 @@
-set DESIGN_NAME Multiplier_unq1
+# the variable $ENABLE_MANUAL_PLACEMENT is used to switch on relative placement
+# one example is running this from command line
+# icc_shell -f multiplier_icc.tcl -x "set ENABLE_MANUAL_PLACEMENT 1;" | tee -i multiplier_icc_optimized.log
 
-set MW_DESIGN_LIBRARY ${DESIGN_NAME}_LIB
+source header.tcl
+
+if {[info exists ENABLE_MANUAL_PLACEMENT]} {
+  set MW_DESIGN_LIBRARY ${DESIGN_NAME}_${VT}_optimized_LIB
+} else {
+  set MW_DESIGN_LIBRARY ${DESIGN_NAME}_${VT}_LIB
+}
+
 if { ![file exists $MW_DESIGN_LIBRARY/lib] } {
        create_mw_lib \
             -tech $TECH_FILE \
@@ -10,53 +19,14 @@ if { ![file exists $MW_DESIGN_LIBRARY/lib] } {
 }
 
 open_mw_lib $MW_DESIGN_LIBRARY
-import_designs $DESIGN_NAME.mapped.v -format verilog -top $DESIGN_NAME -cel $DESIGN_NAME
-read_sdc $DESIGN_NAME.mapped.sdc
+import_designs $DESIGN_NAME.$VT.mapped.v -format verilog -top $DESIGN_NAME -cel $DESIGN_NAME
+read_sdc $DESIGN_NAME.$VT.mapped.sdc
 
 derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT -create_port top
 
-save_mw_cel
-save_mw_cel -as ${DESIGN_NAME}_without_RP
-close_mw_cel
-open_mw_cel ${DESIGN_NAME}_without_RP
+save_mw_cel -as ${DESIGN_NAME}_before_floorplanning
 
-initialize_floorplan \
-  	-control_type aspect_ratio \
-  	-core_aspect_ratio 0.5 \
-  	-core_utilization 0.7 \
-  	-row_core_ratio 1 \
-  	-left_io2core 30 \
-  	-bottom_io2core 30 \
-  	-right_io2core 30 \
-  	-top_io2core 30 \
-  	-start_first_row
-
-if {$TLUPLUS_MIN_FILE == ""} {set TLUPLUS_MIN_FILE $TLUPLUS_MAX_FILE}
-set_tlu_plus_files \
-     -max_tluplus $TLUPLUS_MAX_FILE \
-     -min_tluplus $TLUPLUS_MIN_FILE \
-     -tech2itf_map $MAP_FILE               ;# set the tlu plus files
-
-derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT
-derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT -tie
-
-place_opt  -effort medium -power -area_recovery -num_cpus 2
-
-derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT 
-derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT -tie
-
-route_opt -initial_route_only
-route_opt -skip_initial_route -effort medium -power
-
-save_mw_cel
-close_mw_cel
-
-open_mw_cel ${DESIGN_NAME}
-save_mw_cel -as ${DESIGN_NAME}_with_RP
-close_mw_cel
-open_mw_cel ${DESIGN_NAME}_with_RP
-
-set CSA_cells [get_cells -hierarchical "*csa*"];
+set CSA_cells [get_cells -hierarchical "*add"];
 set max_row 0;
 set max_column 0;
 set max_compressed_column 0;
@@ -78,10 +48,16 @@ foreach_in_collection CSA_cell $CSA_cells {
   }
 }
 
+#get_ports -regexp {[ab][[.[.]].*[[.].]]
+#set_mpc_port_options -side t -order t pin_order
+
+
 # flip rows and columns to keep it more square
 create_rp_group rp_tree -columns [expr $max_row + 1] -rows  [expr $max_compressed_column + 1] -compress;
 
-foreach_in_collection CSA_cell $CSA_cells {
+if {[info exists ENABLE_MANUAL_PLACEMENT]} {
+
+  foreach_in_collection CSA_cell $CSA_cells {
 
     set CSA_name [get_object_name $CSA_cell];
 
@@ -116,7 +92,7 @@ foreach_in_collection CSA_cell $CSA_cells {
         } else {
           foreach_in_collection booth_child_cell $booth_child_cells {
             set booth_child_name [get_object_name $booth_child_cell];
-            add_to_rp_group ${DESIGN_NAME}::rp_booth_csa_${CSA_row}_${compressed_CSA_column} -leaf $booth_child_name -column 0 -row  1;
+            add_to_rp_group ${DESIGN_NAME}::rp_booth_csa_${CSA_row}_${compressed_CSA_column} -leaf $booth_child_name -column 1 -row  0;
             #echo "added leaf  $booth_child_name to rp group rp_booth_csa_${CSA_row}_${compressed_CSA_column}";
           }
         }    
@@ -161,13 +137,12 @@ foreach_in_collection CSA_cell $CSA_cells {
         }
       }    
     }
-}
-
-save_mw_cel -as ${DESIGN_NAME}_before_FP 
+  } 
+} 
 
 initialize_floorplan \
-  	-control_type aspect_ratio \
-  	-core_aspect_ratio 0.5 \
+  	-control_type row_number \
+  	-number_rows [expr $max_compressed_column + 1] \
   	-core_utilization 0.7 \
   	-row_core_ratio 1 \
   	-left_io2core 30 \
@@ -175,6 +150,11 @@ initialize_floorplan \
   	-right_io2core 30 \
   	-top_io2core 30 \
   	-start_first_row
+
+
+save_mw_cel -as ${DESIGN_NAME}_before_placement
+
+
 
 if {$TLUPLUS_MIN_FILE == ""} {set TLUPLUS_MIN_FILE $TLUPLUS_MAX_FILE}
 set_tlu_plus_files \
@@ -185,14 +165,21 @@ set_tlu_plus_files \
 derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT
 derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT -tie
 
+
 place_opt  -effort medium -power -area_recovery -num_cpus 2
+
+save_mw_cel -as ${DESIGN_NAME}_before_routing
 
 derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT 
 derive_pg_connection -power_net $MW_POWER_NET -power_pin $MW_POWER_PORT -ground_net $MW_GROUND_NET -ground_pin $MW_GROUND_PORT -tie
 
+set_rp_group_options [all_rp_groups] -route_opt_option fixed_placement
 route_opt -initial_route_only
 route_opt -skip_initial_route -effort medium -power
 
-save_mw_cel -as ${DESIGN_NAME}_with_RP
-exit
+report_timing -transition_time -nets -attributes -nosplit > reports/${DESIGN_NAME}_with_RP.${VT}_$Voltage.routed.timing.rpt
+
+save_mw_cel -as ${DESIGN_NAME}_final
+
+#exit
 
