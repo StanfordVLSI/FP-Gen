3f45af67056a667b3ee63424f16f2cdd9de8594c
jbrunhav <jbrunhav@jb_co_p4>
2011-12-02 19:01:02 -0800

    small change...
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 9887]

--- synthesis/run_all_32.sh
+++ synthesis/run_all_32.sh
@@ -1,4 +1,4 @@
-./runTarget hvt 0v8 1500 hvt 0v8 1200 hvt 0v8 900 hvt 0v8 700 hvt 0v8 500 \
+./runTarget.pl hvt 0v8 1500 hvt 0v8 1200 hvt 0v8 900 hvt 0v8 700 hvt 0v8 500 \
             svt 0v9 1000 svt 0v9 900 svt 0v9 700 svt 0v9 500 svt 0v9 400 \
             lvt 1v0 700 lvt 1v0 500 lvt 1v0 400 lvt 1v0 350 lvt 1v0 300 lvt 1v0 250
 
