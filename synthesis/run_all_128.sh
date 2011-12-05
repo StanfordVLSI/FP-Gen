e01ea596b5f01a6839ebed766ae665e7b5290eea
jingpu <jingpu@jingpu_p4>
2011-12-04 17:35:46 -0800

    for new synthesis script
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 9918]

--- synthesis/run_all_128.sh
+++ synthesis/run_all_128.sh
@@ -1,4 +1,2 @@
-./runTarget.pl hvt 0v8 3500  hvt 0v8 3000  hvt 0v8 2500  hvt 0v8 2000  hvt 0v8 1500 hvt 0v8 1300 \
-            svt 0v9 1800  svt 0v9 1600  svt 0v9 1400  svt 0v9 1200  svt 0v9 1000 svt 0v9 900 \
-            lvt 1v0 1200  lvt 1v0 1100  lvt 1v0 1000  lvt 1v0  900  lvt 1v0  800  lvt 1v0 700 lvt 1v0 650
+./runTarget.pl lvt 1v0 1200 30  lvt 1v0 1100 30  lvt 1v0 1000 30  lvt 1v0  900 30  lvt 1v0  850 30 lvt 1v0 800 30 lvt 1v0 750 30 
 
