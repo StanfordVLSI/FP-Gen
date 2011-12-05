d385d0937645bd8a401a9f055997b02ac0e39cd2
jingpu <jingpu@jingpu_p4>
2011-12-05 00:33:59 -0800

    tweek parameter range
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 9926]

--- synthesis/run_all_128_noP.sh
+++ synthesis/run_all_128_noP.sh
@@ -1,2 +1,2 @@
-./runTarget.pl lvt 1v0 1400 30  lvt 1v0 1300 30  lvt 1v0 1200 30  lvt 1v0  1100 30  lvt 1v0  1050 30 lvt 1v0 1000 30 lvt 1v0 950 30 
+./runTarget.pl lvt 1v0 1600 30  lvt 1v0 1500 30  lvt 1v0 1400 30  lvt 1v0  1300 30  lvt 1v0  1200 30 lvt 1v0 1050 30 lvt 1v0 1000 30 
 
