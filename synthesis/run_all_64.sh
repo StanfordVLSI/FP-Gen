f11287617fcb20001b7cb5cd0f59449c41c58b7f
jingpu <jingpu@jingpu_p4>
2011-12-13 21:45:11 -0800

    typo fixed
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 9966]

--- synthesis/run_all_64.sh
+++ synthesis/run_all_64.sh
@@ -1,4 +1,4 @@
 ./runTarget.pl hvt 0v8 3500 30 hvt 0v8 3000 30 hvt 0v8 2500 30 hvt 0v8 2000 30 hvt 0v8 1500 30 hvt 0v8 1300 30\
             svt 0v9 1800 30 svt 0v9 1600 30 svt 0v9 1400 30 svt 0v9 1200 30 svt 0v9 1000 30 svt 0v9 900 30\
-            lvt 1v0 1200 30 lvt 1v0 1100 30 lvt 1v0 1000 30 lvt 1v0  900 30 lvt 1v0  800 30 lvt 1v0 700 lvt 1v0 650 30
+            lvt 1v0 1200 30 lvt 1v0 1100 30 lvt 1v0 1000 30 lvt 1v0  900 30 lvt 1v0  800 30 lvt 1v0 700 30 lvt 1v0 650 30
 
