86b76535f790b12ca56e5d43d9c46446e31f1117
jingpu <jingpu@jingpu_p4>
2012-01-18 10:09:51 -0800

    changed minimum target delays
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 9996]

--- synthesis/run_lvt_128.sh
+++ synthesis/run_lvt_128.sh
@@ -1,2 +1,2 @@
-./runTarget.pl lvt 1v0 1200 30  lvt 1v0 1100 30  lvt 1v0 1000 30  lvt 1v0  900 30  lvt 1v0  850 30 lvt 1v0 800 30 lvt 1v0 750 30 
+./runTarget.pl lvt 1v0 1200 30  lvt 1v0 1100 30  lvt 1v0 1000 30  lvt 1v0  950 30  lvt 1v0  900 30 lvt 1v0 850 30 lvt 1v0 800 30 
 
