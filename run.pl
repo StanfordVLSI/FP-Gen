207d75d949a6b257a85ee73c1486a516576826fc
jingpu <jingpu@jingpu_p4>
2012-01-28 21:17:00 -0800

    new release
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 10021]

--- run.pl
+++ run.pl
@@ -1,33 +1,56 @@
 #!/usr/bin/perl
 
-$home_dir = $ENV{'PWD'};
-$xml_dir = $home_dir . "/SysCfgs";
-$synth_dir = $home_dir . "/synthesis";
-$work_dir = $home_dir . "/work";
+use strict;
+use Getopt::Long;
 
-$id = 0 ;
-$tag = sprintf( "%03x" , int( rand( 4095 ) ) ) ;
+my $home_dir = $ENV{'PWD'};
+my $xml_dir = $home_dir . "/SysCfgs";
+my $synth_dir = $home_dir . "/synthesis";
+my $work_dir = $home_dir . "/work";
+
+my $claster = ();
+my $jobs = 50;
+my @xmls = ();
+my @synths = ();
+
+GetOptions(
+	'claster!'	=>	\$claster,
+	'jobs=i'	=>	\$jobs,
+	'xml=s'		=>	\@xmls,
+	'synth=s'	=>	\@synths,
+);
+
+if($#xmls != $#synths){
+	die("The number of XML config files and synthesis scripts do NOT match.");
+}
+
+my $id = 0 ;
+my $tag = sprintf( "%03x" , int( rand( 4095 ) ) ) ;
 
 # go into work dir
 mkdir("$work_dir");
 chdir("$work_dir");
+print "Entering $work_dir\n";
 
 # begin working
-while ( $#ARGV > 0){
-	$xml = shift @ARGV;
-	$synth_scr = shift @ARGV;
+while ( $#xmls >= 0){
+	my $xml = shift @xmls;
+	my $synth_scr = shift @synths;
 
 	# generate and compile .v files
 	mkdir("$xml");
 	chdir("$xml");
-	print "now running makefile...\n";
+	print "Entering $work_dir/$xml\n";
+	print "Now making file for $xml...\n";
 	`make -f $home_dir/Makefile clean all GENESIS_CFG_XML=$xml_dir/$xml`;
 
 	# extract synthesis target
 	open(SYNTH_PAR,"$synth_dir/$synth_scr")||die("Could not open file.\n");	
-	@target = ();
-	while($line = <SYNTH_PAR>){
-		@target = ( @target, split(/[\s\\]+/,$line));
+	my @target = ();
+	while(my $line = <SYNTH_PAR>){
+		my @tmp=( split(/[\s\\]+/,$line));
+		if (length($tmp[0])==0) {shift @tmp;}
+		@target = ( @target, @tmp);
 	}
 	shift(@target);
 	close(SYNTJ_PAR);
@@ -35,16 +58,16 @@ while ( $#ARGV > 0){
 	# generate and run batch jobs
 	`cp -r $synth_dir ./`;
 	chdir ("synthesis");
+	print "Entering $work_dir/$xml/synthesis\n";
 	while (	$#target >0 ){
 	$id++ ;
-	$batchName = "batch_" . $tag . "_n" . $id . ".sh" ;
+	my $batchName = "batch_" . $tag . "_n" . $id . ".sh" ;
 	
-	$s1 = shift @target ;
-	$s2 = shift @target ;
-	$s3 = shift @target ;
-	$s4 = shift @target ;
+	my $s1 = shift @target ;
+	my $s2 = shift @target ;
+	my $s3 = shift @target ;
+	my $s4 = shift @target ;
 
-	print "Running $id --> " . $s1 ."_". $s2 . "_" . $s3 . "\n" ;
 	my $flow = "" ;
 
 	$flow .= "#!/bin/bash  \n";
@@ -54,7 +77,6 @@ while ( $#ARGV > 0){
 	$flow .= " echo \"$id $tag\" \n";
 	$flow .= "   \n";
 	$flow .= "source $synth_dir/synSetup/synopsys_startup/setup.sh \n";
-	$flow .= "echo $SYNOPSYS_RCXT_BIN \n";
 	$flow .= "source $synth_dir/synSetup/synopsys_startup/library_TSMC_45.sh\n";
 	$flow .= "export VCS_ARCH_OVERRIDE=linux   \n";
 	$flow .= "export VCS_HOME=/hd/cad/synopsys/vcs-mx/2010.06   \n";
@@ -69,16 +91,31 @@ while ( $#ARGV > 0){
 	$flow .= "PATH=:/hd/cad/synopsys/star-rcxt/2008.12/amd64_star-rcxt/bin:\$PATH   \n";
 	$flow .= "export GENESIS_LIBS=\"/hd/cad/genesis2/r9648/PerlLibs\"   \n";
 	$flow .= "PATH=\$GENESIS_LIBS/Genesis2:\$PATH   \n";
-	$flow .= "cd \$PBS_O_WORKDIR; make all VT=$s1 Voltage=$s2 target_delay=$s3 io2core=$s4";
+	$flow .= "make all VT=$s1 Voltage=$s2 target_delay=$s3 io2core=$s4";
 	open( BATCHFILE , ">$batchName") ;
 	printf BATCHFILE $flow ;
     	close( BATCHFILE ) ;
 	`chmod 777 $batchName`;
-	`jsub -q horowitz -- ./$batchName`;
-
+	
+	if($claster){
+		my $usrname = $ENV{'USER'};
+		my $count = `qstat | grep -c $usrname`;
+		while ($count >= $jobs){
+			print "Now there are already $count jobs in the queue. The maximum is $jobs. Please wait...\n";
+			sleep(30);
+			my $count = `qstat | grep -c $usrname`;
+		}
+		$count++;
+		`jsub -q horowitz -- ./$batchName`;
+		print "Submitting Job #$id --> " . $s1 ."_". $s2 . "_" . $s3 . "\n" ;
+		print "Job #$id submitted. Now you have $count jobs in the queue.\n";
+	}else{
+		`./$batchName`;
+		print "Running Job #$id --> " . $s1 ."_". $s2 . "_" . $s3 . "\n" ;
+	}
 	}
 	chdir("../");
-
-
+	print "Leaving $work_dir/$xml/synthesis\n";
 	chdir("../");
+	print "Leaving $work_dir/$xml\n";
 }
