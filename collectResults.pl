3209de057217d00e50733dfa718ca0c2d28b456a
jingpu <jingpu@jingpu_p4>
2012-01-29 22:00:51 -0800

    new script for collecting results
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 10023]

---
+++ collectResults.pl
@@ -0,0 +1,43 @@
+#!/usr/bin/perl
+
+use strict;
+use Getopt::Long;
+
+my $home_dir = $ENV{'PWD'};
+my $xml_dir = $home_dir . "/SysCfgs";
+my $synth_dir = $home_dir . "/synthesis";
+my $work_dir = $home_dir . "/work";
+my $result_dir = $home_dir . "/results";
+
+my @xmls = ();
+
+GetOptions(
+        'xml=s'         =>      \@xmls,
+);
+
+
+mkdir ("$result_dir");
+chdir ("$work_dir")||die("There is no work dir.\n");
+print "Entering $work_dir\n";
+
+# begin collecting results
+while ( $#xmls >= 0){
+	my $xml = shift @xmls;
+	chdir ("$xml")||die("There is no $xml dir.\n");
+	print "Entering $work_dir/$xml\n";
+	
+	chdir ("synthesis")||die("There is no synthesis dir in $xml");
+	print "Entering $work_dir/$xml/synthesis\n";
+	
+	my $csv = $xml;
+	$csv =~ s/.xml/.csv/;
+	print "Saving results to $result_dir/$csv...";
+	`./report_results.pl > $result_dir/$csv`;
+	print "done\n";
+	
+        chdir("../");
+        print "Leaving $work_dir/$xml/synthesis\n";
+        chdir("../");
+        print "Leaving $work_dir/$xml\n";
+}
+
