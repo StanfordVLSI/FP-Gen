f56ee761d531bddaa087386ef13602745b429d48
jingpu <jingpu@jingpu_p4>
2012-05-18 14:21:05 -0800

    fixed the ports in top_FMA and Testbench_FMA
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 10546]

--- scripts/RunSingleTest.pl
+++ scripts/RunSingleTest.pl
@@ -3,18 +3,20 @@
 use strict;
 use Getopt::Long;
 
-my $xml = "SysCfgs/ibm_verif/WlcBth2_64.xml";
+my $xml = "SysCfgs/ibm_verif/WlcBth2_64_pipe1.xml";
 my $fpgen_size = 1000;
 my $fpgen_index = 0;
-my $fpgen_seed = int (rand( 10000));
+my $fpgen_seed = 1234;
 my $fpgen_type = "fmadd";
 my $fpgen_model = "ch-5-1-2-3-All-Exponents.fpdef";
+my $run = shift;
+my $name = shift;
 
 my $tag = sprintf( "%03x" , int( rand( 4095 ) ) ) ;
 
 my $home_dir = $ENV{'PWD'};
 my $verif_dir = $home_dir . "/verif_work/";
-my $work_dir = $verif_dir . "batch_". $tag . "/";
+my $work_dir = $verif_dir . $name. "/";
 
 
 GetOptions(
@@ -35,6 +37,8 @@ print "Entering $work_dir\n";
 
 my $make_flags = "GENESIS_CFG_XML=$home_dir/$xml DESIGN_NAME=FMA FPGEN_SEED=$fpgen_seed FPGEN_CLUSTER_SIZE=$fpgen_size FPGEN_CLUSTER_INDEX=$fpgen_index FPGEN_TYPE=$fpgen_type COVERAGE_MODEL=$fpgen_model";
 
+if(!$run){
+
 print "=================================================\n";
 print "=====          Begin Simulation Task        =====\n";
 print "=================================================\n\n";
@@ -45,17 +49,22 @@ print "Random Seed:      $fpgen_seed\n";
 print "Type of Op:       $fpgen_type\n";
 print "FP-Gen model:     $fpgen_model\n\n";
 
-print "Begin Genesis compile phase.\n";
-`make -f $home_dir/Makefile clean comp $make_flags >log/comp.log 2>&1`;
-print "Genesis compile is done.\n\n";
-
 print "Begin Running IBM's fpgen tool.\n";
 `make -f $home_dir/Makefile fpgen $make_flags >log/fpgen.log 2>&1`;
 print "fpgen is done.\n\n";
 
+}else{
+print "Begin Genesis compile phase.\n";
+`make -f $home_dir/Makefile clean comp $make_flags >log/comp.log 2>&1`;
+print "Genesis compile is done.\n\n";
+
+
 print "Begin Running simulation.\n";
 `make -f $home_dir/Makefile run_ibm $make_flags >log/run.log 2>&1`;
 print "Simulation is done.\n\n";
+
+
+`make -f $home_dir/Makefile clean`;
 
 
 print "begin parsing log files.\n";
@@ -64,7 +73,7 @@ my $error = 0;
 open(LOGFILE, "log/comp.log") or die "Can not find log/comp.log";
 while(<LOGFILE>){
     $line++;
-    if($_ =~ m/Error/){
+    if($_ =~ m/(Error)|(ERROR)|(Stop.)/){
 	print "Error found in log/comp.log: Line $line\n";
 	print "=============================\n";
 	print "$_\n";
@@ -77,7 +86,7 @@ $line = 0;
 open(LOGFILE, "log/fpgen.log") or die "Can not find log/fpgen.log";
 while(<LOGFILE>){
     $line++;
-    if($_ =~ m/[Error|FAILED]/){
+    if($_ =~ m/(Error)|(ERROR)|(FAILED)/){
 	print "Error found in log/fpgen.log: Line $line\n";
 	print "=============================\n";
 	print "$_\n";
@@ -90,7 +99,7 @@ $line = 0;
 open(LOGFILE, "log/run.log") or die "Can not find log/run.log";
 while(<LOGFILE>){
     $line++;
-    if($_ =~ m/Error/){
+    if($_ =~ m/(Error)|(ERROR)|(Stop.)/){
 	print "Error found in log/run.log: Line $line\n";
 	print "=============================\n";
 	print "$_\n";
@@ -100,6 +109,8 @@ while(<LOGFILE>){
 close(LOGFILE);
 
 if(!$error){
-    print "Test PASS.\n";
+    print "Test PASS.\n\n";
+}
+
 }
 
