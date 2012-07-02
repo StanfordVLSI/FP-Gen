acbe94fec4472aa33c0724d5c6f97ca4d2abf152
Ofer Shacham <shacham@stanford.edu>
2012-07-02 15:32:57 -0800

    fixed path in libselector
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 10664]

--- synthesis/synSetup/synopsys_startup/libselector.tcl
+++ synthesis/synSetup/synopsys_startup/libselector.tcl
@@ -1,14 +1,14 @@
 
 if {[info exists ENABLE_90]} {
 # ST 90nm
-  source $env(FPGEN)/synthesis/synSetup/synopsys_startup/scr_ST90/common_options.tcl
-  source $env(FPGEN)/synthesis/synSetup/synopsys_startup/scr_ST90/libsetup.tcl -continue_on_error
-  source $env(FPGEN)/synthesis/synSetup/synopsys_startup/scr_ST90/init_psynenv.tcl
+  source $env(FPGEN)/synthesis/synSetup/scr_ST90/common_options.tcl
+  source $env(FPGEN)/synthesis/synSetup/scr_ST90/libsetup.tcl -continue_on_error
+  source $env(FPGEN)/synthesis/synSetup/scr_ST90/init_psynenv.tcl
 } else {
   # TSMC 45nm
-  source $env(FPGEN)/synthesis/synSetup/synopsys_startup/scr_tsmc45/common_options.tcl
-  source $env(FPGEN)/synthesis/synSetup/synopsys_startup/scr_tsmc45/libsetup.tcl -continue_on_error
-  source $env(FPGEN)/synthesis/synSetup/synopsys_startup/scr_tsmc45/init_psynenv.tcl
+  source $env(FPGEN)/synthesis/synSetup/scr_tsmc45/common_options.tcl
+  source $env(FPGEN)/synthesis/synSetup/scr_tsmc45/libsetup.tcl -continue_on_error
+  source $env(FPGEN)/synthesis/synSetup/scr_tsmc45/init_psynenv.tcl
 }
 
 
