1faf0b6f76034183b8901ed187ecbdc854caf7cc
jbrunhav <jbrunhav@jb_co_p4>
2012-08-28 11:41:29 -0800

    updated sample submit script
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 10906]

--- numbers_cfgs/gen_test_smpl.pl
+++ numbers_cfgs/gen_test_smpl.pl
@@ -20,33 +20,35 @@ my $configuration =
           'makefile' => 'Makefile',
           'setupfile' => 'scripts/setup_kiwi.bash',
           'delay_make_variable' => 'SYN_CLK_PERIOD',
-          'make_arguments' => 'USE_SAIF=1 USE_GATE_SAIF=1 USE_ICC_GATE_SAIF=1',
-          'eval_rules' => 'comp run run_dc run_icc',
+          'make_arguments' => 'USE_SAIF=1 USE_GATE_SAIF=1 USE_ICC_GATE_SAIF=1  VERILOG_SIMULATION_FLAGS="+IgnoreErrors"',
+          'eval_rules' => 'comp run run_dc ',
           'cleanup' => 1,
-          'design_title' => 'FMA',
+          'design_title' => 'FPGEN64_DC_v10_',
           'param_make_variable' => 'GENESIS_PARAMS',
           'clean_rules' => 'clean',
           'genesis_hierarchy_variable' => 'GENESIS_HIERARCHY',
           'genesis_cfg_xml_variable' => 'GENESIS_CFG_XML',
           'rollup_rules' => 'rollup1',
           'gen_rules' => 'gen',
-          'runs' => 6,
           'rollup_variable' => 'ROLLUP_TARGET',
-	  'cubby_root' => '/tmp/'   
+	  'cubby_root' => '/tmp/jbrunhaver/runMake/'   
 };
 
 
-#my @trees  = ( 'Wallace', 'Array', 'ZM', 'OS1' ) ;
-my @trees  = ( 'Wallace'  ) ;
-#my @pipes  = ( 3 , 4 , 5 , 6 , 7 , 8 ) ;
-my @pipes  = ( 4 , 5 , 6 , 7 ) ;
-#my @vts    = ( 'lvt' , 'svt' , 'hvt' );
-my @vts    = ( 'lvt' );
+my @trees  = ( 'Wallace', 'Array', 'ZM', 'OS1' ) ;
+#my @trees  = ( 'Wallace'  ) ;
+my @pipes  = ( 3 , 4 , 5 , 6 , 7 , 8 , 9) ;
+#my @pipes  = ( 4 , 5 , 6 , 7 ) ;
+my @vts    = ( 'lvt' , 'svt' , 'hvt' );
+#my @vts    = ( 'lvt' );
 my @delays = ( 0.3 , 0.4 , 0.5 , 0.6 , 0.7 , 0.9 , 1.0 , 1.2 , 1.4 , 1.6 , 1.8 , 2.0 );
-#my @pumps  = ('YES' , 'NO' ) ;
-my @pumps   = ('NO') ;
-#my @booths  = ( 1 , 2 , 3 ) ;
-my @booths  = ( 2 ) ;
+my @pumps  = ('YES' , 'NO' ) ;
+#my @pumps   = ('NO') ;
+my @booths  = ( 1 , 2 , 3 ) ;
+#my @booths  = ( 2 ) ;
+
+my $frac = 52 ;
+my $exp  = 11 ; 
 
 
 my $i = -1 ;
@@ -60,8 +62,8 @@ foreach my $delay (@delays){
     #$i = 0 ;
 
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.Architecture'}                = 'FMA' ;
-    $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.ExponentWidth'}           = 8 ;
-    $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.FractionWidth'}           = 23 ;
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.ExponentWidth'}           = $exp ;
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.FractionWidth'}           = $frac ;
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.PipelineDepth'}           = $pipe;
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.EnableMultiplePumping'}   = $pump;
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.MulShift.MUL0.TreeType'}  = $tree ;
@@ -81,8 +83,8 @@ foreach my $delay (@delays){
     $i += 1 ; 
 
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.Architecture'}                = 'DW_FMA' ;
-    $configuration->{'design'}[$i]{'top_FPGen.FPGen.DW_FMA.ExponentWidth'}        = 8 ;  # FIX ME
-    $configuration->{'design'}[$i]{'top_FPGen.FPGen.DW_FMA.FractionWidth'}        = 23 ;  # FIX ME
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.DW_FMA.ExponentWidth'}        = $exp ;  # FIX ME
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.DW_FMA.FractionWidth'}        = $frac ;  # FIX ME
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.DW_FMA.PipelineDepth'}        = $pipe;
 
     $configuration->{'make_params'}[$i]{'VT'}      = $vt;
@@ -100,8 +102,8 @@ foreach my $delay (@delays){
 
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.Architecture'}                = 'FMA' ;
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.MulShift.MUL0.Designware_MODE'} = 'ON' ;
-    $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.ExponentWidth'}           = 8 ; # FIX ME
-    $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.FractionWidth'}           = 23 ; # FIX ME
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.ExponentWidth'}           = $exp ; # FIX ME
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.FractionWidth'}           = $frac ; # FIX ME
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.FMA.PipelineDepth'}           = $pipe;
 
     $configuration->{'make_params'}[$i]{'VT'}      = $vt;
@@ -111,6 +113,29 @@ foreach my $delay (@delays){
 
 }}}}
 
+foreach my $tree  (@trees){
+foreach my $booth (@booths){
+foreach my $pipe  (@pipes){
+foreach my $vt    (@vts){
+foreach my $pump  (@pumps){
+foreach my $delay (@delays){
+    $i += 1 ; 
+
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.Architecture'}                 = 'CMA' ;
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.ExponentWidth'}            = $exp ; # FIX ME
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.FractionWidth'}            = $frac ; # FIX ME
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.PipelineDepth'}            = $pipe;
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.MUL.Mult.MultP.BoothType'} = $booth ;
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.MUL.Mult.MultP.TreeType'}  = $tree ;
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.EnableMultiplePumping'}    = $pump ;
+
+    $configuration->{'make_params'}[$i]{'VT'}      = $vt;
+    $configuration->{'make_params'}[$i]{'VOLTAGE'} = $vt eq 'lvt' ? '1v0' : ( $vt eq 'svt' ? '0v9' : '0v8' );
+
+    $configuration->{'target_delay'}[$i] = $delay;
+
+}}}}}}
+
 foreach my $pipe  (@pipes){
 foreach my $vt    (@vts){
 foreach my $pump  (@pumps){
@@ -118,9 +143,11 @@ foreach my $delay (@delays){
     $i += 1 ; 
 
     $configuration->{'design'}[$i]{'top_FPGen.FPGen.Architecture'}                = 'CMA' ;
-    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.ExponentWidth'}           = 8 ; # FIX ME
-    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.FractionWidth'}           = 23 ; # FIX ME
-    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.PipelineDepth'}           = $pipe;
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.MUL.Mult.MultP.Designware_MODE'} = 'ON' ;
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.PipelineDepth'}            = $pipe;
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.ExponentWidth'}            = $exp ; # FIX ME
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.FractionWidth'}            = $frac ; # FIX ME
+    $configuration->{'design'}[$i]{'top_FPGen.FPGen.CMA.EnableMultiplePumping'}    = $pump ;
 
     $configuration->{'make_params'}[$i]{'VT'}      = $vt;
     $configuration->{'make_params'}[$i]{'VOLTAGE'} = $vt eq 'lvt' ? '1v0' : ( $vt eq 'svt' ? '0v9' : '0v8' );
