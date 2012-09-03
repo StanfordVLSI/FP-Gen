30c164a4af8061c72b4232b35c1ae768112bcb22
sameh06 <sameh06@sameh06p4>
2012-09-03 12:00:00 -0800

    fix the pipeline depth 0 timing constraint
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 10945]

--- synthesis/multiplier_dc.tcl
+++ synthesis/multiplier_dc.tcl
@@ -132,15 +132,16 @@ if { $PipelineDepth > 0 } {
   set CLK clk
   set CLK_PERIOD [expr double($HEDGE)*double($target_delay)/1000];
   create_clock -name $CLK -period $CLK_PERIOD
-  set_output_delay 0 -clock $CLK  [get_ports "*" -filter {@port_direction == out} ] 
-
+  set_input_delay 0 -clock $CLK [all_inputs]
+  set_output_delay 0 -clock $CLK [all_outputs] 
   set_switching_activity -toggle_rate 0.5 -base_clock $CLK -static_probability 0.5 -type inputs 
   
   eval $COMPILE_COMMAND
 
   set CLK_PERIOD [expr double($target_delay)/1000]
   create_clock -name $CLK -period $CLK_PERIOD
-  set_output_delay 0 -clock $CLK  [get_ports "*" -filter {@port_direction == out} ]
+  set_input_delay 0 -clock $CLK [all_inputs]
+  set_output_delay 0 -clock $CLK [all_outputs] 
   set_switching_activity -toggle_rate 0.5 -base_clock $CLK -static_probability 0.5 -type inputs
 }
 
