eafe48686a89058cd47b3c02651cb8d039b90db4
jingpu <jingpu@jingpu_p4>
2012-09-03 18:48:27 -0800

    add matlab script for plotting multp data
    [git-p4: depot-paths = "//Smart_design/ChipGen/FP-Gen/": change = 10949]

---
+++ scripts/filterCellArray.m
@@ -0,0 +1,23 @@
+% function out = filterCellArray (in, array, value)
+% 	index = find (strcmp(array ,value));
+%     out = cell(size(in));
+%     for i = 1:length(in)
+%         out{i} = in{i}(index);
+%     end
+% end
+
+function out = filterCellArray (varargin)
+    in = varargin{1};
+    out = in;
+    for i = 2:2:nargin
+        array = varargin{i};
+        value = varargin{i+1};
+        index = find (strcmp(array ,value));
+        for i = 1:length(out)
+            out{i} = out{i}(index);
+        end
+        for i = 2:2:nargin
+            varargin{i} = varargin{i}(index);
+        end
+    end
+end
--- scripts/getFrontier.m
+++ scripts/getFrontier.m
@@ -2,17 +2,22 @@ function [xo, yo] = getFrontier (xi, yi)
     [x2, index] = sort(xi);
     y2 = yi(index);
     k = 1;
+    xo = [];
+    yo = [];
     for i = 1:length(x2)
+        if(isnan(y2(i)) || isnan(x2(i)))
+            continue;
+        end
         flag = true;
         for j = 1:length(x2)
-            if ( x2(j) <= x2(i) && y2(j) < y2(i)*0.98)
+            if ( x2(j) <= x2(i) && y2(j) < y2(i) * 1)
                 flag = false;
                 break
             end
         end
         if ( flag == true)
-            xo(k) = x2(i);
-            yo(k) = y2(i);
+            xo(k,1) = x2(i);
+            yo(k,1) = y2(i);
             k = k +1;
         end
     end
