168447968785c160c6fe3815c4083c218a036984
steveri <steveri@steveri.com>
2026-08-28 07:07:00 -0700

    reusable docker containers for librelane

--- scripts/librelane.sh
+++ scripts/librelane.sh
@@ -60,45 +60,40 @@ function INFO {
     echo "INFO-FPGEN: $*"
     echo "=============================================================================="
 }
-
-
-# Default temp name for workspace, container, e.g. "tapeout_3435"
-# testname=$(mktemp -u tapeout_XXXXX)
-testname=$(printf "%04d" $[RANDOM%10000])
-
+# A secret tool that will help us later!
+timestamp=$(date +%m%d-%H%M)       # E.g. "0826_1130"
 
 ##############################################################################
 INFO "Prepare a docker container"
 
-# If user specified a container name, use that; else generate a random name
-[ "$CONTAINER" ] && container="$CONTAINER" || container="tapeout_$testname"
+# If user specified a container name, use that; else use e.g. "tapeout_0826_1130"
+[ "$CONTAINER" ] && container="$CONTAINER" || container="tapeout_$timestamp"
 
 # Make a list of existing librelane containers
 containers=$(docker ps | awk '$2~/librelane/{print $NF}')
 # if [ "$containers" ]; then echo "Found existing librelane container(s)"; echo "$containers" | sed 's/^/    - /'; fi
 
 # If container exists already, then use that
-if grep -q " $container " " $containers "; then
+if grep -q " $container " <<< " $containers "; then
     echo "Will use existing container '$container' as requested"
+    function new_container { false; }
 
 # Else build a new container
 else
     docker run -id --name $container --network host ghcr.io/librelane/librelane:3.0.4 sh
     echo "Built new docker container '$container'"
+    function new_container { true; }
 fi    
-
 
 ##############################################################################
 INFO 'Prepare a workspace e.g. "./tmpdir/tapeout_ZRnNq/"'
 
-testdir=tmpdir/$testname
+testdir=tmpdir/tapeout_$timestamp  # E.g. "tapeout_0826-1130"
 mkdir -p $testdir/rtl
 echo Workspace will be ./$testdir
 
 ##############################################################################
 INFO 'FIND the verilog and add it to the workspace'
-
-
 
 # Copy the verilog to the workspace
 set -x
@@ -107,26 +102,16 @@ cp rtl/dwsub/DWSUB*.v $testdir/rtl/
 rm $testdir/rtl/FPGen*  # Things break if we include the testbench-related files I will file an issue maybe
 set +x
 
-
-INFO 'Install librelane in docker container "$container"'
-
+##############################################################################
 # Install the librelane
-docker exec $container git clone https://github.com/librelane/librelane/ ./librelane
-docker exec $container nix-shell ./librelane/shell.nix
-docker exec $container mkdir -p ./my_designs/fpgen
+if new_container; then
+    INFO 'Install librelane in docker container "$container"'
+    docker exec $container git clone https://github.com/librelane/librelane/ ./librelane
+    docker exec $container nix-shell ./librelane/shell.nix
+    docker exec $container mkdir -p ./my_designs/fpgen
+fi
 
-# INFO 'Install gawk, sed, summarization script in the container'
-
-# docker cp test/summarize_tapeout_log $container:./my_designs/fpgen
-# 
-# # docker exec $container nix-env --install --attr nixpkgs.gawk --dry-run
-# docker exec $container nix-env --install --attr nixpkgs.gawk
-# docker exec $container which awk
-# 
-# # docker exec $container nix-env --install --attr nixpkgs.gnused --dry-run
-# docker exec $container nix-env --install --attr nixpkgs.gnused
-# docker exec $container which sed
-
+##############################################################################
 INFO 'Copy the verilog to the container and build a json config file'
 
 set -x
@@ -146,15 +131,19 @@ echo '{
 docker cp $testdir/fpgen.json $container:./my_designs/fpgen  # Successfully copied 2.05kB
 set +x
 
+##############################################################################
 INFO 'Launch the librelane design flow!'
+set -x
+# log=fpgen.log
+log=fpgen_$timestamp.log  # E.g. "fpgen_0826_1130.log"
+docker exec $container bash -c '(cd ./my_designs/fpgen && librelane fpgen.json |& tee '$log')'
+docker cp $container:./my_designs/fpgen/$log $testdir/$log
+set +x
 
-docker exec $container bash -c '(cd ./my_designs/fpgen && librelane fpgen.json |& tee fpgen.log)'
-docker cp $container:./my_designs/fpgen/fpgen.log $testdir/fpgen.log
-
-
+##############################################################################
 INFO 'Check the result'
 
-if ! egrep -q '^[*] (Antenna|LVS|DRC)' $testdir/fpgen.log; then
+if ! egrep -q '^[*] (Antenna|LVS|DRC)' $testdir/$log; then
     hline="==========================================================="
     echo "ERROR final check(s) not found - looks like we did not make it through the entire test"
     printf "$hline\n\n\n"
@@ -163,7 +152,7 @@ fi
 
 function test_failed { false; }
 for check in Antenna LVS DRC; do
-    result=$(egrep -A 1 "^[*] $check" $testdir/fpgen.log | tr "\n" " ")
+    result=$(egrep -A 1 "^[*] $check" $testdir/$log | tr "\n" " ")
     echo "$result"
     if ! echo "$result" | grep -q Passed; then
         printf "ERROR looks like $check check FAILED\n\n"
@@ -181,21 +170,22 @@ fi
 
 # exec /tmp/libresum.sh: no such file or directory
 
-scripts/libresum.sh $container:./my_designs/fpgen/fpgen.log
+scripts/libresum.sh $container:./my_designs/fpgen/$log
     #             Clock 100.0ns (10MHz)
     #     Critical path  36.89ns
     #        Setup/Hold  42.86ns 40.73ns
     #           WARNING  Max Slew violations found
     #            PASSED  Antenna LVS DRC
 
-
+##############################################################################
 INFO 'GDS-II tape is here maybe:'
 
-gds=$(sed -n '/Writing out GDS/,/INFO..Done/p' $testdir/fpgen.log \
+gds=$(sed -n '/Writing out GDS/,/INFO..Done/p' $testdir/$log \
   | tr -d '\n' | sed "s/^[^']*[']//" | sed "s/['].*//")
 echo docker exec $container ls -lh $gds
 docker exec $container ls -lh $gds
 
+##############################################################################
 INFO 'Cleanup
     To clean up:
         docker kill $container
